5c31008af10095d4d2903e3ebf633a2edeb7bb4d
AFSAL333 <afsalafs8008@gmail.com>
2025-03-21 12:22:44 +0530

    favicon added

--- src/components/ui/floating-navbar.jsx
+++ src/components/ui/floating-navbar.jsx
@@ -53,7 +53,7 @@ export const FloatingNav = ({ navItems, className }) => {
         {navItems.map((navItem, idx) => (
           <Link
             key={`link=${idx}`}
-            href={navItem.link}
+            to={navItem.link}
             className={cn(
               "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
